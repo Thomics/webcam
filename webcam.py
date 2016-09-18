b98975b4d037f339ae42095ad6606aac7ec81397
Thomics <contot@gmail.com>
2016-09-17 22:34:46 -0700

    updated file name

--- webcam.py
+++ webcam.py
@@ -1,15 +1,17 @@
-import time, os
+import time, os, datetime
 
 count = 0
+img_list = []
 
 #name - name of the photo, num is the iteration
 def take_photo(interval, name, num):
 	print(name + " " + str(num))
-
 	cur_dir = os.getcwd()
-
-	os.system("fswebcam -r 640x480 --no-banner " + cur_dir + "/photos/" + name + str(num))
+	time = datetime.datetime.now()
+	os.system("fswebcam -r 640x480 --no-banner " + cur_dir + "/photos/" + name + str(num) + '-' + str(time.year) + '-' + str(time.month) + '-' + str(time.day))
+	img_list.extend(name + str(num) + '-' + str(time.year) + '-' + str(time.month) + '-' + str(time.day))
 	time.sleep(interval)
+
 
 
 while True:
