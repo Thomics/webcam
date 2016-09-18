8f014eafff1c62504da857a6c2c76a6605b68999
Thomics <contot@gmail.com>
2016-09-17 19:47:55 -0700

    initial commit

---
+++ webcam.py
@@ -0,0 +1,14 @@
+import time, os
+
+count = 0
+
+#name - name of the photo, num is the iteration
+def take_photo(interval, name, num):
+	print(name + " " + str(num))
+	os.system("fswebcam -r 640x480 " + name + str(num))
+	time.sleep(interval)
+
+
+while True:
+	count = count + 1
+	take_photo(3, 'boat', count)
