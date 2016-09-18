b9ac2200175af55b62e784c2712d74f1da8b884c
Thomics <contot@gmail.com>
2016-09-18 14:30:18 -0700

    file path broken

--- webcam.py
+++ webcam.py
@@ -1,17 +1,15 @@
 import time, os, datetime
 
 count = 0
-img_list = []
 
 #name - name of the photo, num is the iteration
 def take_photo(interval, name, num):
 	cur_dir = os.getcwd()
 	cur_time = datetime.datetime.now()
 	#Generates the photo that will display on the page.
-	os.system("fswebcam -r 1024x768 --no-banner " + cur_dir + '/' + 'boat-life.jpg')
+	# os.system("fswebcam -r 1024x768 --no-banner " + cur_dir + '/' + 'boat-life.jpg')
 	#Stores all photos created in a photos folder.
-	os.system("fswebcam -r 1024x768 --no-banner " + cur_dir + "/photos/" + name + str(num) + '-' + str(cur_time.year) + '-' + str(cur_time.month) + '-' + str(cur_time.day) + '.jpg')
-	img_list.extend(name + str(num) + '-' + str(cur_time.year) + '-' + str(cur_time.month) + '-' + str(cur_time.day) + '.jpg')
+	os.system("fswebcam -r 640x480 --no-banner " + cur_dir + "/photos/" + name + str(num) + '-' + str(cur_time.year) + '-' + str(cur_time.month) + '-' + str(cur_time.day) + '.jpg')
 	time.sleep(interval)
 #end take_photo
 
